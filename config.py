1dfaee0c01e4c4e076d3beba5be8cbf930f2b4fc
Abhinav <abhinav810singh@gmail.com>
2026-04-15 09:54:11 +0100

    Add config, gitignore and .env files

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SNOWFLAKE_CONFIG = {
+    "user": os.getenv("SNOWFLAKE_USER"),
+    "password": os.getenv("SNOWFLAKE_PASSWORD"),
+    "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+}
